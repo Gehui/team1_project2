c1fc2fb49033e0d11f9595b98111243c68140ba5
benjaminmgross <benjaminmgross@yahoo.com>
2014-07-21 01:18:40 -0400

    Added functionality to run year-based-regression using Random Forest Regression Trees

--- analyze.py
+++ analyze.py
@@ -12,15 +12,70 @@ Data Science Course
 import argparse
 import pandas
 import numpy
+import sys
 import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn import ensemble
 from sklearn import cluster
+from sklearn import linear_model
 
 #these seemed like the important columns, so I made the variable global
-COLS = ['G', 'G_batting', 'AB', 'R', 'H', 'X2B', 'X3B', 'HR', 'RBI',
-        'SB', 'CS', 'BB', 'SO', 'IBB', 'HBP', 'SH', 'SF', 'GIDP']
-    
+COLS = ['G_batting', 'AB', 'R', 'H', 'X2B', 'X3B', 'HR', 'RBI',
+        'SB', 'CS', 'BB', 'SO', 'IBB', 'HBP', 'SH', 'SF', 'GIDP', 'teamID',
+        'salary', 'yearID']
+
+def year_based_significance_regression(file_path):
+    """
+    Run a year-based multivariate regression that uses only the significant variables
+    as well as Random Forest Regression Trees to estimate the parameters
+
+    Args:
+    ------
+    - file_path: string of the location of `baseball.csv`
+
+    Returns:
+    ---------
+    - pandas.DataFrame of the in-sample and out-of-sample salary estimates
+    """
+
+    data = pandas.DataFrame.from_csv(file_path, index_col = None)
+    data['age'] = data['yearID'] - data['birthYear']
+    cols = COLS
+    cols.append('age')
+    all_data = data[cols].copy()
+    all_data.dropna(inplace = True)
+    teams = pandas.get_dummies(all_data['teamID'])
+    x_cols = all_data.columns[map(lambda x: x not in ['teamID', 'salary'], all_data.columns)]
+    xs = all_data[x_cols].join(teams)
+    ys = all_data['salary']
+    N = xs.shape[0]
+    isi, in_sample, osi, out_sample = create_in_out_samples(xs, N/2)
+    d = {}
+    for year in all_data['yearID'].unique():
+        no_yr = in_sample.columns.drop('yearID')
+        d_too = {}
+        is_yr = in_sample['yearID'] == year
+        os_yr = out_sample['yearID'] == year
+        ols = pandas.ols(x = in_sample.loc[is_yr, no_yr], y = ys[isi][is_yr])
+        df = ols.summary_as_matrix
+        is_sig = df.loc['p-value', df.loc['p-value', :] < .01].index
+
+        if 'intercept' in is_sig:
+            is_sig = is_sig.drop('intercept')
+
+        clf = ensemble.RandomForestRegressor(n_estimators = 15)
+        clf.fit(in_sample.loc[is_yr, is_sig], ys[isi][is_yr])
+        print "For year " + str(year)
+        is_score = clf.score(in_sample.loc[is_yr, is_sig], ys[isi][is_yr])
+        print "in-sample" + '\t' + str(is_score)
+        d_too['is-r2'] = is_score
+        os_score = clf.score(out_sample.loc[os_yr, is_sig], ys[osi][os_yr])
+        print "out-of-sample" + '\t' + str(os_score)
+        d_too['os-r2'] = os_score
+        d[year] = pandas.Series(d_too)
+
+    return pandas.DataFrame(d).transpose()
+
 def load_baseball_data(file_path):
     """
     Return a `pandas.DataFrame` of the baseball data
@@ -32,7 +87,7 @@ def load_baseball_data(file_path):
     Returns:
     --------
     `pandas.DataFrame` of the data
-    
+
     """
     return pandas.DataFrame.from_csv(file_path, index_col = None)
 
@@ -56,7 +111,7 @@ def create_in_out_samples(data, in_sample_size):
     """
     #in-sample index and out-of-sample index
     isi = numpy.random.choice(data.index, in_sample_size)
-    osi = data.index[~data.index.isin(isi)] 
+    osi = data.index[~data.index.isin(isi)]
 
     #create in-sample and out-of-sample DataFrames
     in_sample = data.loc[ isi, :].copy()
@@ -88,7 +143,7 @@ def mv_regression(xs, ys, in_sample_size):
     Returns:
     ---------
     float of the MSE or Mean Squared Error
-    
+
     """
     isi, in_sample, osi, out_sample = create_in_out_samples(xs, in_sample_size)
 
@@ -104,13 +159,13 @@ def mv_regression(xs, ys, in_sample_size):
     mse = eps.sum()/( eps.shape[0] - 2)
 
     return mse
-    
+
 
 def pc_regression(xs, ys, in_sample_size, var_target):
     """
     Construct a multivariate regression using the principal components
     that explain the var_target of variation
-    
+
     Args:
     -----
     - xs: `pandas.DataFrame` of the xs
@@ -123,7 +178,7 @@ def pc_regression(xs, ys, in_sample_size, var_target):
     Returns:
     ---------
     float of the MSE or Mean Squared Error
-     
+
     """
     isi, in_sample, osi, out_sample = create_in_out_samples(xs, in_sample_size)
 
@@ -142,8 +197,32 @@ def pc_regression(xs, ys, in_sample_size, var_target):
     pred = pc_os.dot(betas) + intercept
     eps = (pred - ys[osi]).apply(numpy.abs)
     mse = eps.sum()/(eps.shape[0] - 2)
-    
+
     return mse
+
+def sklearn_mv_regression(xs, ys, in_sample_size):
+    """
+    Using `sklearn` Regression Trees
+
+    Args:
+    -----
+    - xs: `pandas.DataFrame` of the xs
+    - ys: `pandas.Series` of the variable we're attempting to predit
+    - in_sample_size: integer of the size of the data to train the model on
+
+    Returns:
+    ---------
+    float of the MSE or Mean Squared Error
+
+    """
+    d = {'MAE':[], 'in-sample-r2':[], 'out-sample-r2':[]}
+
+    isi, in_sample, osi, out_sample = create_in_out_samples(xs, in_sample_size)
+    clf = linear_model.LinearRegression(fit_intercept = True)
+    clf.fit(in_sample, ys[isi])
+    d['in-sample-r2'] = clf.score(in_sample, ys[isi])
+    pred = clf.predict(out_sample, ys[osi])
+    d['']
 
 def regression_tree(xs, ys, max_depth, in_sample_size):
     """
@@ -159,7 +238,7 @@ def regression_tree(xs, ys, max_depth, in_sample_size):
     Returns:
     ---------
     float of the MSE or Mean Squared Error
-    
+
     """
     isi, in_sample, osi, out_sample = create_in_out_samples(xs, in_sample_size)
     clf = tree.DecisionTreeRegressor(max_depth = max_depth)
@@ -184,8 +263,8 @@ def regression_forest(xs, ys, num_classifiers, in_sample_size):
     Returns:
     ---------
     float of the MSE or Mean Squared Error
-    
-    
+
+
     """
     isi, in_sample, osi, out_sample = create_in_out_samples(xs, in_sample_size)
     clf = ensemble.RandomForestRegressor(num_classifiers)
@@ -206,7 +285,7 @@ def cluster_then_forest(xs, ys, in_sample_size):
         print "Now working on Cluster " + str(id)
         oos_ind = oos_clusterid == id
         ins_ind = ins_clusterid == id
-        
+
         tree = ensemble.RandomForestRegressor(50)
 
         tree.fit(in_sample[ins_ind], ys[isi][ins_ind])
@@ -218,8 +297,8 @@ def cluster_then_forest(xs, ys, in_sample_size):
         print str(tree.score(out_sample[oos_ind], ys[osi][oos_ind]))
 
     return None
-        
-        
+
+
 
 def compare_functions(xs, ys, num_sims, in_sample_size):
     """
@@ -237,6 +316,7 @@ def compare_functions(xs, ys, num_sims, in_sample_size):
     --------
     `pandas.DataFrame` of the different MSE for each of the functions
     """
+
     d = {'mv_regression':[],
          'pc_regression': [],
          'regression_tree':[],
@@ -259,15 +339,14 @@ def compare_functions(xs, ys, num_sims, in_sample_size):
 
     return pandas.DataFrame(d)
 
-def scipt_function(arg_1, arg_2):
-	return None
-
 if __name__ == '__main__':
-	
+
 	usage = sys.argv[0] + "usage instructions"
-	description = "describe the function"
+	description = "This is my silly little analyze function"
 	parser = argparse.ArgumentParser(description = description, usage = usage)
 	parser.add_argument('name_1', nargs = 1, type = str, help = 'describe input 1')
 	parser.add_argument('name_2', nargs = '+', type = int, help = "describe input 2")
+
 	args = parser.parse_args()
+
 	script_function(input_1 = args.name_1[0], input_2 = args.name_2)
